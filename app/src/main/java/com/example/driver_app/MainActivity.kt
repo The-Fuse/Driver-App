56de7897c2b13b1a03326940d6083a05ae822550
The-Fuse <rohitkumaryadav141002@gmail.com>
2020-09-04 22:53:34 +0530

    Finally added the location part and saved the location on database

--- app/src/main/java/com/example/driver_app/MainActivity.kt
+++ app/src/main/java/com/example/driver_app/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.driver_app
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.startActivity
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -14,12 +22,16 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_registeration.*
+import java.util.jar.Manifest
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startmyday.setOnClickListener {
+            checkbusno()
+        }
         supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#ffffff")))
         val user = Firebase.auth.currentUser
         if (user != null){
@@ -36,10 +48,40 @@ class MainActivity : AppCompatActivity() {
             .addOnSuccessListener { document ->
                 if (document !=null){
                     val username = document.get("name")
-                    supportActionBar?.setTitle("Hello ${username}")
+                    supportActionBar?.setTitle("Hello $username")
                 }
             }
     }
+    private fun checkbusno(){
+        val busno = bus_number.text.toString()
+        if (busno.isNotEmpty()) {
+            Firebase.firestore.collection("Buses").get().addOnSuccessListener { documents ->
+                for (document in documents) {
+                    if (busno == document.id) {
+                        val driverassigned = document.get("DriverAssigned")
+                        if (driverassigned == false) {
+                            val uid  = Firebase.auth.currentUser?.uid
+                            val ref = Firebase.firestore.collection("Buses").document(document.id)
+                            ref.update("DriverAssigned",true,"driver_id",uid)
+                            val intent = Intent(this,home_page::class.java)
+                            startActivity(intent)
+                        } else {
+                            Snackbar.make(
+                                startmyday,
+                                "Driver already Assigned !!",
+                                Snackbar.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+
+
+            }
+        }else{
+            Snackbar.make(startmyday, "Please enter the bus no.", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menuaction, menu)
@@ -59,3 +101,4 @@ class MainActivity : AppCompatActivity() {
     }
 
 }
+class Locationdata(val longitude: String, val latitude: String)
---
+++ app/src/main/java/com/example/driver_app/home_page.kt
@@ -0,0 +1,70 @@
+package com.example.driver_app
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_home_page.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class home_page : AppCompatActivity() {
+    lateinit var locationmanager : LocationManager
+    lateinit var location : Location
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+        if (ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                1
+            )
+        }
+            locationmanager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            if (locationmanager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+                location = locationmanager.getLastKnownLocation(LocationManager.GPS_PROVIDER)!!
+            }else{
+                location = locationmanager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)!!
+            }
+            val locationlistener = object: LocationListener {
+                override fun onLocationChanged(p0: Location) {
+                    reversegeoCode(p0)
+                }
+
+            }
+        if (locationmanager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            locationmanager.requestLocationUpdates(LocationManager.GPS_PROVIDER,100,100.2f,locationlistener)
+
+        }else{
+            locationmanager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,100,100.2f,locationlistener)
+
+        }
+
+    }
+
+    private fun reversegeoCode(location: Location?) {
+        val uid  = Firebase.auth.currentUser?.uid.toString()
+        val loca = Locationdata(location?.longitude.toString(), location?.latitude.toString())
+        if (location != null) {
+            Firebase.firestore.collection("Drivers").document(uid).update("Longitude",location.longitude,"Latitude",location.latitude)
+                .addOnSuccessListener {
+                    Snackbar.make(button2, "Location Data feeds start", Snackbar.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener {
+                    Snackbar.make(button2, "Failed location feed", Snackbar.LENGTH_SHORT).show()
+                }
+        }
+    }
+}
