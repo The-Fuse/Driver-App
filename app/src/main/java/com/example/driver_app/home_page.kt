3b487ec580339877a8953ba4bf38af5e9778db3d
The-Fuse <rohitkumaryadav141002@gmail.com>
2020-09-04 23:34:47 +0530

    Finally added the location part and saved the location on database

--- app/src/main/java/com/example/driver_app/home_page.kt
+++ app/src/main/java/com/example/driver_app/home_page.kt
@@ -2,25 +2,31 @@ package com.example.driver_app
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_home_page.*
-import kotlinx.android.synthetic.main.activity_main.*
 
 class home_page : AppCompatActivity() {
     lateinit var locationmanager : LocationManager
     lateinit var location : Location
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#ffffff")))
+        supportActionBar?.setTitle("Your Bus Details")
         if (ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED
             && ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(
@@ -60,10 +66,10 @@ class home_page : AppCompatActivity() {
         if (location != null) {
             Firebase.firestore.collection("Drivers").document(uid).update("Longitude",location.longitude,"Latitude",location.latitude)
                 .addOnSuccessListener {
-                    Snackbar.make(button2, "Location Data feeds start", Snackbar.LENGTH_SHORT).show()
+                    Snackbar.make(takeabreak, "Location Data feeds start", Snackbar.LENGTH_SHORT).show()
                 }
                 .addOnFailureListener {
-                    Snackbar.make(button2, "Failed location feed", Snackbar.LENGTH_SHORT).show()
+                    Snackbar.make(takeabreak, "Failed location feed", Snackbar.LENGTH_SHORT).show()
                 }
         }
     }
