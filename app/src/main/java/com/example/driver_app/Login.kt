b1d4cdad3664df2c452d69f6c36da01da4935b75
The-Fuse <rohitkumaryadav141002@gmail.com>
2020-09-04 16:58:29 +0530

    Completed login and registration process

--- app/src/main/java/com/example/driver_app/Login.kt
+++ app/src/main/java/com/example/driver_app/Login.kt
@@ -3,15 +3,42 @@ package com.example.driver_app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_registeration.*
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        supportActionBar?.hide()
         registrationtransfer.setOnClickListener {
             val intent = Intent(this,Registeration::class.java)
             startActivity(intent)
+            finish()
         }
+        button.setOnClickListener {
+            login()
+        }
+    }
+    private fun login(){
+        val email = login_email.text.toString()
+        val password = login_password.text.toString()
+        if (email.isEmpty() || password.isEmpty()){
+            Snackbar.make(button, "Please Fill out the field!!", Snackbar.LENGTH_SHORT).show()
+            return
+        }
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener {
+                Snackbar.make(button, "Logged in Successfully !!", Snackbar.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+                intent.flags =
+                    Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+                startActivity(intent)
+                finish()
+            }
     }
 }
--- app/src/main/java/com/example/driver_app/MainActivity.kt
+++ app/src/main/java/com/example/driver_app/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.driver_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#ffffff")))
+        val user = Firebase.auth.currentUser
+        if (user != null){
+            Snackbar.make(fdfgd, "Logged in Successfully !!", Snackbar.LENGTH_SHORT).show()
+        }else {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menuaction, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.Logout -> {
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this, Login::class.java)
+                startActivity(intent)
+                finish()
+                true
+            }else->super.onOptionsItemSelected(item)
+        }
+    }
+
 }
