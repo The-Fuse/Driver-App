96ce81d4d116b676fbcbd71299d0e4283db7d7cc
The-Fuse <rohitkumaryadav141002@gmail.com>
2020-09-06 11:53:33 +0530

    Fixed all the bugs

--- app/src/main/java/com/example/driver_app/home_page.kt
+++ app/src/main/java/com/example/driver_app/home_page.kt
@@ -1,6 +1,9 @@
 package com.example.driver_app
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
@@ -10,67 +13,155 @@ import android.location.LocationManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.annotation.RequiresApi
-import androidx.core.app.ActivityCompat
+import android.provider.Settings
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_home_page.*
 
-class home_page : AppCompatActivity() {
-    lateinit var locationmanager : LocationManager
-    lateinit var location : Location
 
+private const val PERMISSION_REQUEST = 10
+class home_page : AppCompatActivity() {
+    lateinit var locationManager: LocationManager
+    private var hasGps = false
+    private var hasNetwork = false
+    private var locationGps: Location? = null
+    private var locationNetwork: Location? = null
+    private var permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
         supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#ffffff")))
         supportActionBar?.setTitle("Your Bus Details")
-        if (ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED
-            && ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(
-                    android.Manifest.permission.ACCESS_FINE_LOCATION,
-                    android.Manifest.permission.ACCESS_COARSE_LOCATION
-                ),
-                1
-            )
-        }
-            locationmanager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-            if (locationmanager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
-                location = locationmanager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)!!
-            }else{
-                location = locationmanager.getLastKnownLocation(LocationManager.GPS_PROVIDER)!!
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkPermission(permissions)) {
+                getLocation()
+            } else {
+                requestPermissions(permissions, PERMISSION_REQUEST)
             }
-            val locationlistener = object: LocationListener {
-                override fun onLocationChanged(p0: Location) {
-                    reversegeoCode(p0)
-                }
-
-            }
-        if (locationmanager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
-            locationmanager.requestLocationUpdates(LocationManager.GPS_PROVIDER,100,100.2f,locationlistener)
-
-        }else{
-            locationmanager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,100,100.2f,locationlistener)
-
+        } else {
+            getLocation()
         }
 
     }
 
-    private fun reversegeoCode(location: Location?) {
-        val uid  = Firebase.auth.currentUser?.uid.toString()
-        val loca = Locationdata(location?.longitude.toString(), location?.latitude.toString())
-        if (location != null) {
-            Firebase.firestore.collection("Drivers").document(uid).update("Longitude",location.longitude,"Latitude",location.latitude)
-                .addOnSuccessListener {
-                    Snackbar.make(takeabreak, "Location Data feeds start", Snackbar.LENGTH_SHORT).show()
+    @SuppressLint("MissingPermission")
+    private fun getLocation() {
+        val uid = Firebase.auth.currentUser?.uid
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        hasGps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        hasNetwork = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        if (hasGps || hasNetwork) {
+
+            if (hasGps) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0F, object : LocationListener {
+                    override fun onLocationChanged(p0: Location) {
+                        if (p0 != null) {
+                            locationGps = p0
+                            if (uid != null) {
+                                Firebase.firestore.collection("Drivers").document(uid).update("Longitude",
+                                    locationGps!!.longitude,"Latitude", locationGps!!.latitude)
+                                    .addOnSuccessListener {
+                                        Snackbar.make(takeabreak, "Location Data feeds start", Snackbar.LENGTH_SHORT).show()
+                                    }
+                                    .addOnFailureListener {
+                                        Snackbar.make(takeabreak, "Failed location feed", Snackbar.LENGTH_SHORT).show()
+                                    }
+                            }
+                        }
+                    }
+
+                })
+
+                val localGpsLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                if (localGpsLocation != null)
+                    locationGps = localGpsLocation
+            }
+            if (hasNetwork) {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 0F, object : LocationListener {
+                    override fun onLocationChanged(p0: Location) {
+                        if (p0 != null) {
+                            locationNetwork = p0
+                            if (uid != null) {
+                                Firebase.firestore.collection("Drivers").document(uid).update("Longitude",
+                                    locationNetwork!!.longitude,"Latitude", locationNetwork!!.latitude)
+                                    .addOnSuccessListener {
+                                        Snackbar.make(takeabreak, "Location Data feeds start", Snackbar.LENGTH_SHORT).show()
+                                    }
+                                    .addOnFailureListener {
+                                        Snackbar.make(takeabreak, "Failed location feed", Snackbar.LENGTH_SHORT).show()
+                                    }
+                            }
+                        }
+                    }
+
+                })
+
+                val localNetworkLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+                if (localNetworkLocation != null)
+                    locationNetwork = localNetworkLocation
+            }
+
+            if(locationGps!= null && locationNetwork!= null){
+                if(locationGps!!.accuracy > locationNetwork!!.accuracy){
+                    if (uid != null) {
+                        Firebase.firestore.collection("Drivers").document(uid).update("Longitude",
+                            locationGps!!.longitude,"Latitude", locationGps!!.latitude)
+                            .addOnSuccessListener {
+                                Snackbar.make(takeabreak, "Location Data feeds start", Snackbar.LENGTH_SHORT).show()
+                            }
+                            .addOnFailureListener {
+                                Snackbar.make(takeabreak, "Failed location feed", Snackbar.LENGTH_SHORT).show()
+                            }
+                    }
+                }else{
+                    if (uid != null) {
+                        Firebase.firestore.collection("Drivers").document(uid).update("Longitude",
+                            locationNetwork!!.longitude,"Latitude", locationNetwork!!.latitude)
+                            .addOnSuccessListener {
+                                Snackbar.make(takeabreak, "Location Data feeds start", Snackbar.LENGTH_SHORT).show()
+                            }
+                            .addOnFailureListener {
+                                Snackbar.make(takeabreak, "Failed location feed", Snackbar.LENGTH_SHORT).show()
+                            }
+                    }
                 }
-                .addOnFailureListener {
-                    Snackbar.make(takeabreak, "Failed location feed", Snackbar.LENGTH_SHORT).show()
+            }
+
+        } else {
+            startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+        }
+    }
+    private fun checkPermission(permissionArray: Array<String>): Boolean {
+        var allSuccess = true
+        for (i in permissionArray.indices) {
+            if (checkCallingOrSelfPermission(permissionArray[i]) == PackageManager.PERMISSION_DENIED)
+                allSuccess = false
+        }
+        return allSuccess
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST) {
+            var allSuccess = true
+            for (i in permissions.indices) {
+                if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                    allSuccess = false
+                    val requestAgain = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && shouldShowRequestPermissionRationale(permissions[i])
+                    if (requestAgain) {
+                        Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "Go to settings and enable the permission", Toast.LENGTH_SHORT).show()
+                    }
                 }
+            }
+            if (allSuccess)
+                getLocation()
+
         }
     }
 }
+
